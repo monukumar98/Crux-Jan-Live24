864ec474b5af12206f5aefd327025ff5e258848c
monukumar98 <monu012as@gmail.com>
2024-02-23 21:35:25 +0530

    Time and complexity

---
+++ Lec17/Time_Sapce_Comp.java
@@ -0,0 +1,119 @@
+package Lec17;
+
+public class Time_Sapce_Comp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		long start = System.currentTimeMillis();
+//		for (int i = 0; i < 1000000; i++) {
+//
+//		}
+//		long end = System.currentTimeMillis();
+//		System.out.println(end - start);
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		int n = 100;
+		int i = 0;
+		while (i < n) {
+			System.out.println("Hey");
+			// O(N)
+			i++;
+		}
+		while (i <= n) {
+			System.out.println("Hey");
+			// Log(N)
+
+			i *= 2;
+		}
+		while (n > 0) {
+			System.out.println("Hey");
+			// Log(N)
+			n /= 2;
+		}
+		while (i <= n) {
+			System.out.println("Hey");
+
+			i += 2;
+			i += 3;
+		}
+		while (i <= n) {
+			System.out.println("Hey");
+			// O(log(N)) base 6
+			i *= 2;
+			i *= 3;
+		}
+
+		while (n > 0) {
+			System.out.println("Hey");
+			// O(log(N)) base 6
+			n /= 2;
+			n /= 3;
+		}
+		int k = 1;
+		while (i <= n) {
+			System.out.println("Hey");
+			// linear time loop n/k times chalega
+			i += k;
+		}
+		while (i <= n) {
+			System.out.println("Hey");
+			// log me time aayega base k
+			i *= k;
+		}
+
+		while (n > 0) {
+			System.out.println("Hey");
+
+			n = n - 1;
+		}
+		while (n > 0) {
+			System.out.println("Hey");
+
+			n = n - 2;
+			n = n - 3;
+		}
+		while (n > 0) {
+			n = n - k;
+		}
+
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				System.out.println("hey");
+				// O(N^2)
+			}
+		}
+
+		for (i = 1; i * i <= n; i++) {
+			// O(Sqrt(N))
+			System.out.println("hey");
+		}
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= i * i; j++) {
+				for (k = 1; k <= n / 2; k++) {
+					// O(n^4)
+					System.out.println("hey");
+				}
+			}
+		}
+		for (i = 1; i <= n; i *= 2) {
+			System.out.println("hey");
+			// log n base 2
+		}
+		for (i = n / 2; i <= n; i++) {
+			for (int j = 1; j <= n / 2; j++) {
+				for (k = 1; k <= n; k = k * 2) {
+					System.out.println("hey");
+					// O(N^2) log (N)
+				}
+			}
+		}
+		for (i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j += i) {
+				System.out.println("hey");
+			}
+		}
+	}
+
+}
