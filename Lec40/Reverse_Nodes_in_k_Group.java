92ef889a3abd3b1cecc4bb9db5a0fd7349c0b2f7
monukumar98 <monu012as@gmail.com>
2024-05-11 10:13:04 +0530

    LinkedlIst Qps

--- Lec40/Reverse_Nodes_in_k_Group.java
+++ Lec40/Reverse_Nodes_in_k_Group.java
@@ -1,10 +1,50 @@
 package Lec40;
 
+import java.util.Stack;
+
 public class Reverse_Nodes_in_k_Group {
+	public class ListNode {
+		int val;
+		ListNode next;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ListNode() {
+		}
 
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	class Solution {
+		public ListNode reverseKGroup(ListNode head, int k) {
+			Stack<ListNode> st = new Stack<>();
+			ListNode Dummy = new ListNode();
+			ListNode temp = Dummy;
+			ListNode start = head;
+			while (head != null) {
+				st.push(head);
+				head = head.next;
+				if (st.size() == k) {
+					while (!st.isEmpty()) {
+						Dummy.next = st.pop();
+						Dummy = Dummy.next;
+
+					}
+					start = head;
+				}
+			}
+			Dummy.next = null;
+			if (st.size() > 0) {
+				Dummy.next = start;
+			}
+			return temp.next;
+
+		}
 	}
 
 }
