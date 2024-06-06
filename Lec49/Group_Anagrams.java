094fd6f5ffec2b261220e1a9f2a9d1f46e5f97cf
monukumar98 <monu012as@gmail.com>
2024-06-06 21:18:33 +0530

    Graph-1 BFS & DFS

--- Lec49/Group_Anagrams.java
+++ Lec49/Group_Anagrams.java
@@ -38,7 +38,7 @@ public class Group_Anagrams {
 		}
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < freq.length; i++) {
-			sb.append(freq[i]);
+			sb.append(freq[i]+" ");
 		}
 		return sb.toString();
 
